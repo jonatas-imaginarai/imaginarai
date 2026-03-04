6ae86b34e95f1cef0d1e806fafc81dc98d49e542
Jonatas Freire Giordano <jonatas.imaginar.ai@gmail.com>
2026-03-03 23:32:43 -0300

    v3.4 - Add New Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { PortfolioSection } from './components/PortfolioSection'
 import { CtaBanner } from './components/CtaBanner'
 import { HowItWorks } from './components/HowItWorks'
 import { BlogSection } from './components/BlogSection'
-import { TeamSection } from './components/TeamSection'
 import { ContactSection } from './components/ContactSection'
 import { Footer } from './components/Footer'
 import { Separator } from './components/Separator'
 import { TeamSectionV2 } from './components/TeamSectionV2'
+import { FlickeringGrid } from './components/ui/flickering-grid'
+
+
 
 function App() {
 
@@ -43,8 +45,20 @@ function App() {
         colorButtonCta='violet'
       />
       <ContactSection />
+      <div className="bg-background relative h-[500px] w-full overflow-hidden rounded-lg border">
+      <FlickeringGrid
+        className="absolute inset-0 z-0 size-full"
+        squareSize={4}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={800}
+        width={800}
+      />
+    </div>
       <Footer />
-      
+
     </>
   )
 }
--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,16 +1,30 @@
 import { ContactButtonsHeroSection } from "../ContactButtonsHeroSection"
-import icon1 from '../../assets/icons/i-30.png'
+import DancingLetters from "../dancing-letters"
+import { AuroraText } from "../ui/aurora-text"
+import { FlickeringGrid } from "../ui/flickering-grid"
 
-const styleHeroSection = 'flex flex-col min-h-[80%] gap-6 items-center justify-center text-center mt-2 border w-[100%] rounded-sm px-10 py-12 bg-gradient-to-b from-white to-blue-50'
-const titleHero = 'font-bold text-3xl w-1/2'
-const subTitleHero = 'w-1/2 text-stone-700'
+const styleHeroSection = 'bg-background relative flex flex-col min-h-[80%] gap-6 items-center justify-center text-center mt-2 border w-[100%] rounded-sm px-10 py-12 bg-gradient-to-b from-white to-blue-50'
+const titleHero = 'font-bold text-stone-700 text-xl w-[80%]'
+/* const subTitleHero = 'w-1/2 text-stone-700' */
 const ctaHeroSection = 'duration-200 ease-in-out text-stone-700 rounded-full bg-sky-300 px-10 py-2 shadow-lg font-bold cursor-pointer hover:bg-sky-200 active:scale-[95%] active:bg-sky-100 active:shadow-xl text-sm'
 
-export function HeroSection () {
+export function HeroSection() {
     return (
         <div className={styleHeroSection}>
-            <h2 className={titleHero}>Diseñamos Soluciones De Inteligencia Artificial a Medida Para Tu Negocio</h2>
-            <h3 className={subTitleHero}>Sistemas Inteligentes Para Hacer Crecer Tu Empresa</h3>
+            <FlickeringGrid
+                className="absolute pointer-events-none w-full inset-0 z-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]"
+                squareSize={4}
+                gridGap={6}
+                color="#80ccff"
+                maxOpacity={0.3}
+                flickerChance={0.1}
+                height={800}
+            />
+            <DancingLetters
+                text='imaginar.ai'
+            />
+            <h2 className={titleHero}>Soluciones de <AuroraText>Inteligencia Artificial</AuroraText> a Medida Para Tu Negocio</h2>
+            {/* <h3 className={subTitleHero}>Sistemas Inteligentes Para Hacer Crecer Tu Empresa</h3> */}
             <button className={ctaHeroSection}>Agendar Reunión</button>
             <ContactButtonsHeroSection />
 
